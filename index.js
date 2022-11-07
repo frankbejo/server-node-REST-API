e43a36307976310b19cd86da06339d045a412243
frankbejo <frankyenbejo@gmail.com>
2022-11-07 23:01:12 +0800

    new commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv/config')
 
 
 app.use(cors({
-    origin: "https://jandf-clothing.vercel.app"
+    origin: "http://localhost:3000"
 }))
 // middlewares
 // app.use('/products', () => {
